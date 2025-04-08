dbffaf3aa8c5cac045bd6513f82b49da205b107e
Will Ware <wware@alum.mit.edu>
2025-04-08 12:20:24 -0400

    camelCase is just too ugly

--- example.py
+++ example.py
@@ -5,5 +5,5 @@ def add(x: int, y: int) -> int:
     return x + y
 def multiply(x: int, y: int) -> int:
     return x * y
-def dotProduct(w: int, x: int, y: int, z: int) -> int:
+def dot_product(w: int, x: int, y: int, z: int) -> int:
     return add(multiply(w, y), multiply(x, z))
--- test_example.py
+++ test_example.py
@@ -1,3 +1,3 @@
 from example import dotProduct
 def test_dot_product():
-    assert dotProduct(314, 159, 262, 323) == 133625
+    assert dot_product(314, 159, 262, 323) == 133625
